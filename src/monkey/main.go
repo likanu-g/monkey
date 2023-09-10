1dd407cd1b6d15a09424e2559cf797e0e58d822d
likanu-g <1452930742@qq.com>
2023-09-10 09:43:16 +0800

    增加抽象语法书的定义

--- src/monkey/main.go
+++ src/monkey/main.go
@@ -8,10 +8,7 @@ import (
 )
 
 func main() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
+	user, _ := user.Current()
 	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
 	fmt.Printf("Feel free to type in commands\n")
 	repl.Start(os.Stdin, os.Stdout)
